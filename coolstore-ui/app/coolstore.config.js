4089edbb6da6d17f31f9edbbf4d392168c78e4f7
jamesfalkner <schtool@gmail.com>
2016-08-12 16:24:55 -0400

    minor cleanup

--- coolstore-ui/app/coolstore.config.js
+++ coolstore-ui/app/coolstore.config.js
@@ -1,7 +1,6 @@
 var config =
 {
-  REST_ENDPOINT: 'https://secure-pricing-testsso2.shadowman.com/coolstore-1.0.0/rest'
-
+  REST_ENDPOINT: process.env.REST_ENDPOINT || 'https://secure-pricing-testsso2.shadowman.com/coolstore-1.0.0/rest'
 };
 
 module.exports = config;
--- coolstore-ui/app/keycloak.config.js
+++ coolstore-ui/app/keycloak.config.js
@@ -1,12 +1,9 @@
-var pk = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArUCsfdVvwyo1L9FZyOsioPhKjHXoearkvvQfWraa88xLhw1f69cPsO8iG2d8nUNG5sOuvzJ/2HuO+c3oTxpLkeReYiZFt/KnmJmT0o3QVzq/oJOzKPVCg/nXgcWQBLX4vj9tTQ0rXsinANuk3f2qCNwkFiV2d/ELllyI+qKs+P8NfKSK4VVi8OkuhyMeYr4f8jZXbtdciErW9brnjUUNkoX0QraxIMvLF8rje1JaxUwxHuMVFlBJ9Ix4RBsjOA4/wdd/VhVaH2fnpwPxvz0WcaiH5gxR3aAmPKPSKl44Ru8sQPmDjfscfOBjmyZOCRbF0aj2aQVLyxGiX+LaIVGZWQIDAQAB';
-
-
 var config =
 {
   "auth-server-url" : process.env.SSO_URL || 'https://secure-sso-testsso2.shadowman.com/auth',
   "realm": process.env.SSO_REALM || 'myrealm',
-  "realm-public-key": process.env.SSO_PUBLIC_KEY || pk,
-  "resource": 'coolstore-ui',
+  "realm-public-key": process.env.SSO_PUBLIC_KEY || '',
+  "resource": process.env.SSO_CLIENT_ID || 'coolstore-ui',
   "ssl-required": 'external',
   "public-client": true
 };
