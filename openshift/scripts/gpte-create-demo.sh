20b0e5bf3126576c0b43ad80828c98a3a03582f2
Siamak Sadeghianfar <siamaksade@gmail.com>
2016-12-30 11:27:47 +0100

    provisioning scripts waits for nexus before creating apps

--- openshift/scripts/gpte-create-demo.sh
+++ openshift/scripts/gpte-create-demo.sh
@@ -27,14 +27,19 @@ GITHUB_REF=demo-1
 # DEPLOY FUNCTIONS                                            #
 ###############################################################
 
-# Create Projects
-function create_projects() {
-  echo_header "Creating projects..."
+# Create Infra Project
+function create_infra_project() {
+  echo_header "Creating infa project..."
+  oc new-project cicd-$PROJECT_SUFFIX --display-name='CI/CD Infra' --description='CI/CD Infra Environment'
+}
+
+# Create Application Project
+function create_app_projects() {
+  echo_header "Creating application projects..."
   oc new-project coolstore-test-$PROJECT_SUFFIX --display-name='Coolstore TEST' --description='Coolstore Test Environment'
   oc new-project coolstore-stage-$PROJECT_SUFFIX --display-name='Coolstore STAGE' --description='Coolstore Staging Environment'
   oc new-project coolstore-prod-$PROJECT_SUFFIX --display-name='Coolstore PROD' --description='Coolstore Production Environment'
   oc new-project inventory-dev-$PROJECT_SUFFIX --display-name='Inventory DEV' --description='Inventory Dev Environment'
-  oc new-project cicd-$PROJECT_SUFFIX --display-name='CI/CD Infra' --description='CI/CD Infra Environment'
 }
 
 # Extract domain and set DOMAIN variable
@@ -47,44 +52,57 @@ function extract_and_set_domain() {
 # Deploy Gogs
 function deploy_gogs() {
   echo_header "Deploying Gogs git server..."
-  sleep 5
   oc process -f https://raw.githubusercontent.com/OpenShiftDemos/gogs-openshift-docker/master/openshift/gogs-persistent-template.yaml -v HOSTNAME=gogs-cicd-$PROJECT_SUFFIX.$DOMAIN | oc create -f - -n cicd-$PROJECT_SUFFIX
 }
 
 # Deploy Nexus
 function deploy_nexus() {
   echo_header "Deploying Sonatype Nexus repository manager..."
-  sleep 5
   oc process -f https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-persistent-template.yaml | oc create -f -
+}
+
+# Wait till Nexus is ready
+function wait_for_nexus_to_be_ready() {
+  echo_header "Waiting for Nexus to be ready..."
+  x=1
+  while [ -z "$(oc get ep nexus -o yaml | grep '\- addresses:')" ]
+  do
+    echo "."
+    sleep 5
+    x=$(( $x + 1 ))
+    if [ $x -gt 120 ]
+    then
+      echo "Nexus still not ready, I GIVE UP!"
+      exit 255
+    fi
+  done
+  echo "Nexus is ready!"
 }
 
 # Deploy Jenkins
 function deploy_jenkins() {
   echo_header "Deploying Jenkins..."
-  sleep 5
   oc new-app jenkins-persistent -l app=jenkins -p JENKINS_PASSWORD=openshift -n cicd-$PROJECT_SUFFIX
 }
 
 # Deploy Coolstore into Coolstore TEST project
 function deploy_coolstore_test_env() {
   echo_header "Deploying CoolStore app into coolstore-test-$PROJECT_SUFFIX project..."
-  sleep 5
   oc process -f https://raw.githubusercontent.com/$GITHUB_ACCOUNT/coolstore-microservice/$GITHUB_REF/openshift/coolstore-template.yaml -v GIT_REF=$GITHUB_REF -v MAVEN_MIRROR_URL=http://nexus.cicd-$PROJECT_SUFFIX.svc.cluster.local:8081/content/groups/public | oc create -f - -n coolstore-test-$PROJECT_SUFFIX
 }
 
 # Deploy Inventory Service into Inventory DEV project
 function deploy_inventory_service() {
   echo_header "Deploying Inventory service into inventory-dev-$PROJECT_SUFFIX project..."
-  sleep 5
   oc process -f https://raw.githubusercontent.com/$GITHUB_ACCOUNT/coolstore-microservice/$GITHUB_REF/openshift/services/inventory-service.json -v GIT_REF=$GITHUB_REF -v MAVEN_MIRROR_URL=http://nexus.cicd-$PROJECT_SUFFIX.svc.cluster.local:8081/content/groups/public | oc create -f - -n inventory-dev-$PROJECT_SUFFIX
 }
 
 function set_user_as_admin() {
-  oc adm policy add-role-to-user admin $UID -n coolstore-test-$PROJECT_SUFFIX
-  oc adm policy add-role-to-user admin $UID -n coolstore-stage-$PROJECT_SUFFIX
-  oc adm policy add-role-to-user admin $UID -n coolstore-prod-$PROJECT_SUFFIX
-  oc adm policy add-role-to-user admin $UID -n inventory-dev-$PROJECT_SUFFIX
-  oc adm policy add-role-to-user admin $UID -n cicd-$PROJECT_SUFFIX
+  oc adm policy add-role-to-user admin $USER_ID -n coolstore-test-$PROJECT_SUFFIX
+  oc adm policy add-role-to-user admin $USER_ID -n coolstore-stage-$PROJECT_SUFFIX
+  oc adm policy add-role-to-user admin $USER_ID -n coolstore-prod-$PROJECT_SUFFIX
+  oc adm policy add-role-to-user admin $USER_ID -n inventory-dev-$PROJECT_SUFFIX
+  oc adm policy add-role-to-user admin $USER_ID -n cicd-$PROJECT_SUFFIX
 }
 
 # GPTE convention
@@ -104,12 +122,16 @@ function echo_header() {
 ###############################################################
 # DEPLOY DEMO                                                 #
 ###############################################################
-create_projects
+create_infra_project
 extract_and_set_domain
-deploy_gogs
 deploy_nexus
+deploy_gogs
 deploy_jenkins
+
+create_app_projects
+wait_for_nexus_to_be_ready
 deploy_coolstore_test_env
 deploy_inventory_service
+
 set_default_project
 set_user_as_admin # must be the last not to interfere with user quota during provisioning
