572b73f08eaa5375bae99651f5b4005d6b30b7c6
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-01-12 11:59:11 +0100

    parameterized catalog endpoint

--- cart-service-eap/src/main/java/com/redhat/coolstore/service/ShoppingCartServiceImpl.java
+++ cart-service-eap/src/main/java/com/redhat/coolstore/service/ShoppingCartServiceImpl.java
@@ -17,13 +17,13 @@ import feign.jackson.JacksonDecoder;
 
 @Stateless
 public class ShoppingCartServiceImpl implements ShoppingCartService {
-	
+
 	@Inject
 	ShippingService ss;
-	
+
 	@Inject
 	PromoService ps;
-	
+
 	private Map<String, ShoppingCart> cartDB = new HashMap<>();
 
 	private Map<String, Product> productMap = new HashMap<>();
@@ -43,30 +43,30 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 	public void priceShoppingCart(ShoppingCart sc) {
 
 		if ( sc != null ) {
-			
+
 			initShoppingCartForPricing(sc);
-			
+
 			if ( sc.getShoppingCartItemList() != null && sc.getShoppingCartItemList().size() > 0) {
-			
+
 				ps.applyCartItemPromotions(sc);
-				
+
 				for (ShoppingCartItem sci : sc.getShoppingCartItemList()) {
-					
+
 					sc.setCartItemPromoSavings(sc.getCartItemPromoSavings() + sci.getPromoSavings() * sci.getQuantity());
 					sc.setCartItemTotal(sc.getCartItemTotal() + sci.getPrice() * sci.getQuantity());
-					
+
 				}
-				
-				ss.calculateShipping(sc);				
-				
+
+				ss.calculateShipping(sc);
+
 			}
-			
+
 			ps.applyShippingPromotions(sc);
-			
+
 			sc.setCartTotal(sc.getCartItemTotal() + sc.getShippingTotal());
-		
+
 		}
-		
+
 	}
 
 	private void initShoppingCartForPricing(ShoppingCart sc) {
@@ -78,30 +78,31 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 		sc.setCartTotal(0);
 
 		for (ShoppingCartItem sci : sc.getShoppingCartItemList()) {
-			
+
 			Product p = getProduct(sci.getProduct().getItemId());
-			
+
 			//if product exist, create new product to reset price
 			if ( p != null ) {
-			
+
 				sci.setProduct(new Product(p.getItemId(), p.getName(), p.getDesc(), p.getPrice()));
 				sci.setPrice(p.getPrice());
 			}
-			
+
 			sci.setPromoSavings(0);
-			
+
 		}
-		
-		
+
+
 	}
 
 	@Override
 	public Product getProduct(String itemId) {
 		if (!productMap.containsKey(itemId)) {
 
+			String catalogEndpoint = System.getenv("CATALOG_ENDPOINT")
 			CatalogService cat = Feign.builder()
 					.decoder(new JacksonDecoder())
-					.target(CatalogService.class, "http://catalog-service:8080");
+					.target(CatalogService.class, catalogEndpoint == null ? "http://catalog:8080" : catalogEndpoint);
 
 			// Fetch and cache products. TODO: Cache should expire at some point!
 			List<Product> products = cat.products();
