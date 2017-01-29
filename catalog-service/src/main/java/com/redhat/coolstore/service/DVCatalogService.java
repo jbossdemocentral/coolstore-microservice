ce90b17c82f1b7fb806619521f1e0e788598e280
Cojan van Ballegooijen <cvanball@redhat.com>
2017-01-29 23:26:44 +0100

    Added extra logging information

--- catalog-service/src/main/java/com/redhat/coolstore/service/DVCatalogService.java
+++ catalog-service/src/main/java/com/redhat/coolstore/service/DVCatalogService.java
@@ -85,8 +85,8 @@ public class DVCatalogService implements CatalogService {
         }
 
         StringBuilder odataUrl = new StringBuilder();
-        odataUrl.append("http://").append(hostName).append(":").append(portNo).append("/").append(vdbName).append("/").append(schemaName);
-
+        odataUrl.append("http://").append(hostName).append(":").append(portNo).append("/odata4/").append(vdbName).append("/").append(schemaName);
+        log.info("OData URL: "+odataUrl.toString());
 
         oc.getConfiguration().setHttpClientFactory(new BasicAuthHttpClientFactory(userName,password));
 		URI absoluteUri = oc.newURIBuilder(odataUrl.toString()).appendEntitySetSegment(entitySetName).build();
--- catalog-service/src/main/java/com/redhat/coolstore/utils/Producers.java
+++ catalog-service/src/main/java/com/redhat/coolstore/utils/Producers.java
@@ -26,6 +26,7 @@ public class Producers {
 
     @Produces @ApplicationScoped
     public ODataClient createODataClient() {
+            log.info("Creating ODataClient");
     		return new ODataClientImpl();
     }
 
