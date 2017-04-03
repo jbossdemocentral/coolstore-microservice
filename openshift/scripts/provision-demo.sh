fc311996942240c3c3856388e3850761f2ec2ece
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-04-03 14:08:36 +0700

    fixed current directory issue

--- openshift/scripts/provision-demo.sh
+++ openshift/scripts/provision-demo.sh
@@ -603,6 +603,7 @@ if [ "$LOGGEDIN_USER" == 'system:admin' ] && [ -z "$ARG_USERNAME" ] ; then
   fi
 fi
 
+pushd ~
 START=`date +%s`
 
 echo_header "Multi-product MSA Demo ($(date))"
@@ -645,6 +646,7 @@ case "$ARG_COMMAND" in
 esac
 
 set_default_project
+popd ~
 
 END=`date +%s`
 echo
