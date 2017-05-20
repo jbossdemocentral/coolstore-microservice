e66f186af7e1b81c855eef67ab2a1da46ab4fe67
Bernard Tison <bernard.tison@gmail.com>
2017-05-20 15:26:45 +0200

    Configurable Shopping Cart Service implementation

--- cart-service/src/main/java/com/redhat/coolstore/service/ShoppingCartServiceImpl.java
+++ cart-service/src/main/java/com/redhat/coolstore/service/ShoppingCartServiceImpl.java
@@ -7,13 +7,11 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.redhat.coolstore.model.Product;
 import com.redhat.coolstore.model.ShoppingCart;
 import com.redhat.coolstore.model.ShoppingCartItem;
 
-@Component
 public class ShoppingCartServiceImpl implements ShoppingCartService {
 	
 	@Autowired
