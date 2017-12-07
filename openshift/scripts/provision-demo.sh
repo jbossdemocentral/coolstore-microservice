b03fa79be47d771986e88733e7594ae834734f91
Siamak Sadeghianfar <siamaksade@gmail.com>
2017-12-07 16:56:33 +0100

    fixed jenkins image tag ref

--- openshift/scripts/provision-demo.sh
+++ openshift/scripts/provision-demo.sh
@@ -454,9 +454,12 @@ function deploy_jenkins() {
   echo_header "Deploying Jenkins..."
 
   # import jenkins image tags
-  oc import-image jenkins:v3.7 --from="registry.access.redhat.com/openshift3/jenkins-2-rhel7" --confirm -n openshift 2>/dev/null
-  sleep 10
-  oc new-app jenkins-ephemeral -l app=jenkins -p MEMORY_LIMIT=1Gi --param=JENKINS_IMAGE_STREAM_TAG=v3.7 -n ${PRJ_CI[0]}
+  if [ $LOGGEDIN_USER == 'system:admin' ] ; then
+    oc import-image jenkins:v3.7 --from="registry.access.redhat.com/openshift3/jenkins-2-rhel7" --confirm -n openshift 2>/dev/null
+    sleep 10
+  fi
+  
+  oc new-app jenkins-ephemeral -l app=jenkins -p MEMORY_LIMIT=1Gi --param=JENKINS_IMAGE_STREAM_TAG=jenkins:v3.7 -n ${PRJ_CI[0]}
   sleep 2
   oc set resources dc/jenkins --limits=cpu=1,memory=2Gi --requests=cpu=200m,memory=1Gi -n ${PRJ_CI[0]}
 }
